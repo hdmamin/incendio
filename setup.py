c19eda1f2cc231281eb67c2b149fcb9d8a8d9d35
hdmamin <hmamin2@dons.usfca.edu>
2020-11-20 22:19:54 -0800

    notes

--- setup.py
+++ setup.py
@@ -14,10 +14,12 @@ for o in expected: assert o in cfg, "missing expected setting: {}".format(o)
 setup_cfg = {o:cfg[o] for o in cfg_keys}
 
 licenses = {
-    'apache2': ('Apache Software License 2.0','OSI Approved :: Apache Software License'),
+    'apache2': ('Apache Software License 2.0',
+                'OSI Approved :: Apache Software License'),
 }
-statuses = [ '1 - Planning', '2 - Pre-Alpha', '3 - Alpha',
-    '4 - Beta', '5 - Production/Stable', '6 - Mature', '7 - Inactive' ]
+statuses = ['1 - Planning', '2 - Pre-Alpha', '3 - Alpha',
+            '4 - Beta', '5 - Production/Stable',
+            '6 - Mature', '7 - Inactive']
 py_versions = '2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 3.0 3.1 3.2 3.3 3.4 3.5 3.6 3.7 3.8'.split()
 
 requirements = cfg.get('requirements','').split()
@@ -32,16 +34,18 @@ setuptools.setup(
         'Intended Audience :: ' + cfg['audience'].title(),
         'License :: ' + lic[1],
         'Natural Language :: ' + cfg['language'].title(),
-    ] + ['Programming Language :: Python :: '+o for o in py_versions[py_versions.index(min_python):]],
-    url='https://github.com/{}/{}'.format(cfg['user'],cfg['lib_name']),
+    ] + ['Programming Language :: Python :: '+o
+         for o in py_versions[py_versions.index(min_python):]],
+    url='https://github.com/{}/{}'.format(cfg['user'], cfg['lib_name']),
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=requirements,
     extras_require={'gg': ['accio']},
-    python_requires ='>=' + cfg['min_python'],
+    python_requires='>=' + cfg['min_python'],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     zip_safe=False,
-    entry_points={ 'console_scripts': cfg.get('console_scripts','').split() },
+    # entry_points={'console_scripts': cfg.get('console_scripts', '').split()},
+    entry_points={'console_scripts': ['incendio=incendio.cli:generate']}, # TODO: may need to uninstall and reinstall to test this
     **setup_cfg)
 
